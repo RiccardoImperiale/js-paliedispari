405d35b401b149ecb2c22c924ed780f07f9e6ee7
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-27 15:08:59 +0100

    add random number function

--- main.js
+++ main.js
@@ -13,4 +13,25 @@ function isPalindrome(word) {
 
 // console.log(isPalindrome(userWord));
 
-// the user chooses ever or odd
+
+// Even or Odd --------------------------------------------------------------------->
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    // get user choice between even and odd
+    const userChoice = document.querySelector('.btn-check:checked').value;
+    // get user number choice 
+    const userNumber = Number(document.querySelector('#userNumber').value)
+    console.log(userChoice);
+    console.log(userNumber);
+
+})
+
+// generate a random number from 1 to 5 for the computer
+function getRandNumFrom1To5() {
+    return Math.floor(Math.random() * 5) + 1;
+}
+
+console.log(getRandNumFrom1To5());
