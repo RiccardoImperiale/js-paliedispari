30db471c4d8d396037ae8b98cf7313c5754fb14a
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-02-27 14:02:43 +0100

    simplified solution

--- main.js
+++ main.js
@@ -2,16 +2,8 @@
 const userWord = prompt('Enter a word:');
 // create a function to see if the entered word is a palindrome
 function isPalindrome(word) {
-    let isPalindrome;
-    let firstHalfWord = '';
-    let lastHalfWord = '';
     let reversedWord = word.split("").reverse().join("");
-
-    for (let i = 0; i < word.length / 2; i++) {
-        firstHalfWord += word[i];
-        lastHalfWord += reversedWord[i];
-    }
-    return firstHalfWord === lastHalfWord ? isPalindrome = true : isPalindrome = false
+    return word === reversedWord;
 }
 
 console.log(isPalindrome(userWord));
